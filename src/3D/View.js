09236912c88cee6cb95fc07e17c46a09eb1bc22b
huhuhu <84453264+h-uhun@users.noreply.github.com>
2025-11-25 00:22:25 +0900

    Model-View間の通知処理を改善

--- src/3D/View.js
+++ src/3D/View.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { EVENTS } from '../events.js';
 
 export class View {
 
@@ -7,19 +8,23 @@ export class View {
         this.COLS = 10;
         this.ROWS = 20;
         this.BLOCK_SIZE = 30;
-        // this.canvas = document.getElementById('game-canvas');
-        // this.ctx = this.canvas.getContext('2d');
-        // this.initCanvas();
 
         this.cubes = [];
         this.geometry = new THREE.BoxGeometry( 1, 1, 1 );
 
+        
         this.initCore();
         this.initLights();
         this.initEnvironment();
         this.initControls();
 
         this.renderLoop();
+        this.handlers = {
+            [EVENTS.UPDATE_BOARD]: (data) => this.draw(data),
+            [EVENTS.GAME_OVER]:        () => this.gameOver(),
+            [EVENTS.SCORE_CHANGED]:(data) => console.log(data),
+            [EVENTS.IS_PAUSED]:    (data) => console.log(data)
+        };
     }
 
     initCore() {
@@ -39,7 +44,6 @@ export class View {
         this.light.position.set(-1, 2, 4);
         this.light.castShadow = true;
         
-        // 影の調整
         this.light.shadow.camera.left = -50;
         this.light.shadow.camera.right = 50;
         this.light.shadow.camera.top = 50;
@@ -51,7 +55,6 @@ export class View {
     }
     
     initEnvironment() {
-        // 地面
         const planeGeometry = new THREE.PlaneGeometry( 1000, 1000 );
         const planeMaterial = new THREE.MeshPhongMaterial({ 
             color: 0x808080,
@@ -63,17 +66,14 @@ export class View {
         this.ground.receiveShadow = true;
         this.scene.add(this.ground);
         
-        // 壁（共通設定）
         const wallGeometry = new THREE.BoxGeometry(50, 20, 1);
         const wallMaterial = new THREE.MeshPhongMaterial({ color: 0x808080 });
         
-        // 左の壁
         this.leftWall = new THREE.Mesh(wallGeometry, wallMaterial);
         this.leftWall.castShadow = true;
         this.leftWall.position.set(-25.5, -9.5, 0);
         this.scene.add(this.leftWall);
 
-        // 右の壁
         this.rightWall = new THREE.Mesh(wallGeometry, wallMaterial);
         this.rightWall.castShadow = true;
         this.rightWall.position.set(34.5, -9.5, 0);
@@ -86,38 +86,15 @@ export class View {
         this.controls.update();
     }
 
-    // キャンバスの初期化 2d
-    // initCanvas(){
-    //     if (this.ctx) {
-    //         this.ctx.strokeStyle = 'rgba(0, 0, 0, 0.1)';
-    //         this.ctx.lineWidth = 1;
-    //     } else {
-    //         console.error('コンテキストの取得に失敗しました。');
-    //     }
-    // }
-
-        // Modelからデータを受信
-    update(data){
-        this.draw(data);
+    update(event, data){
+        if (this.handlers[event]) {
+            this.handlers[event](data);
+        } else {
+            console.warn(`[${event}]は登録されていません。`);
+        }
     }
 
     draw(board){
-        //2d
-        // this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-        // this.drawGrid();
-
-        // board.forEach((row, y) => {
-        //     row.forEach((value, x) => {
-        //         if (value) {
-        //             this.drawBlock(x, y, value);
-        //         }
-        //     });
-        // });
-        //2d
-        
-        //ここから3D
-        //cubesを全部消してcubeをpushする。
         this.cubes.forEach(cube => this.scene.remove(cube));
         this.cubes = [];
         board.forEach((row, y) => {
@@ -149,28 +126,4 @@ export class View {
         this.cubes.push(cube);
         return cube;
     }
-    
-    // drawBlock(x, y, color, context = this.ctx) {
-    //     context.fillStyle = color;
-    //     context.fillRect(x * this.BLOCK_SIZE, y * this.BLOCK_SIZE, this.BLOCK_SIZE, this.BLOCK_SIZE);
-    //     context.strokeStyle = '#000';
-    //     context.strokeRect(x * this.BLOCK_SIZE, y * this.BLOCK_SIZE, this.BLOCK_SIZE, this.BLOCK_SIZE);
-    // }
-
-    //2d
-    // drawGrid(){
-    //     for (let x = 0; x <= 10; x++) {
-    //         this.ctx.beginPath();
-    //         this.ctx.moveTo(x * 30, 0);
-    //         this.ctx.lineTo(x * 30, 20 * 30);
-    //         this.ctx.stroke();
-    //     }
-
-    //     for (let y = 0; y <= 20; y++) {
-    //         this.ctx.beginPath();                                                                                           
-    //         this.ctx.moveTo(0, y * 30);
-    //         this.ctx.lineTo(10 * 30, y * 30);
-    //         this.ctx.stroke();
-    //     }
-    // }
 }
